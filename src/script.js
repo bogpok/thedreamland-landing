1794a2d52d455abd98b784cec88b0eb3678b1c35
Bogdan Pokrepin <bogpok@ya.ru>
2024-09-30 17:15:07 +0300

    i18next localization library

--- src/script.js
+++ src/script.js
@@ -16,3 +16,35 @@ function toggleLyrics(id='', target_btn=undefined) {
 
     target_btn.classList.add('btn-pressed');
 }
+
+// LOCALIZATION
+i18next
+.use(i18nextHttpBackend) 
+.use(i18nextBrowserLanguageDetector)
+.init({
+    fallbackLng: 'en', // Default languageб
+    debug: true,
+    backend: {
+        loadPath: './langs/{{lng}}.json'
+    }   
+    
+}, function(err, t) {
+    updateContent(t);
+});
+
+function updateContent(t) {
+    // Loop through translation keys and update elements with matching IDs
+    const translationKeys = Object.keys(i18next.store.data[i18next.language].translation);
+    translationKeys.forEach(function (key) {
+      const element = document.getElementById(key);
+      if (element) {
+        element.innerText = t(key); // Set the translated text
+      }
+    });
+}
+
+function changeLanguage(lang) {
+    i18next.changeLanguage(lang, function(err, t) {
+        updateContent(t);
+    });
+}
